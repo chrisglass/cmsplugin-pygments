8c37f8df297193382f4a8c355ca492dbf0a24871
fivethreeo <oyvind.saltvik@gmail.com>
2010-12-13 18:52:49 +0100

    setup py fixing

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "cmsplugin-pygments",
+    version = "0.7.0",
+    url = 'http://github.com/fivethreeo/cmsplugin-pygments',
+    license = 'BSD',
+    description = "django-cms plugin for pygments",
+    author = 'Oyvind Saltvik',
+    author_email = 'oyvind.saltvik@gmail.com',
+    packages = find_packages('.'),
+    package_dir = {'':'.'},
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    include_package_data=True,
+    zip_safe = False
+)
