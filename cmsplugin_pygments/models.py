ae5a30cc57134b9babda04b4f2bc258e66b4150a
fivethreeo <oyvind.saltvik@gmail.com>
2010-12-14 11:58:54 +0100

    added language choices improved getting styles

--- cmsplugin_pygments/models.py
+++ cmsplugin_pygments/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from cms.models import CMSPlugin
-from pygments.styles import STYLE_MAP
+from pygments.styles import get_all_styles
+from pygments.lexers import get_all_lexers 
 
-STYLE_CHOICES = map(lambda x: (x,x), STYLE_MAP.keys())
+STYLE_CHOICES = map(lambda x: (x,x), get_all_styles())
+
+LANGUAGE_CHOICES = map(lambda x: (x[0],x[1][0]), get_all_lexers())
+LANGUAGE_CHOICES.sort(lambda x,y: cmp(x[0], y[0]))
 
 class PygmentsPlugin(CMSPlugin):
-    code_language = models.CharField(max_length=20)
+    code_language = models.CharField(max_length=20, choices=LANGUAGE_CHOICES)
     code = models.TextField()
     style = models.CharField(max_length=255, choices=STYLE_CHOICES)
